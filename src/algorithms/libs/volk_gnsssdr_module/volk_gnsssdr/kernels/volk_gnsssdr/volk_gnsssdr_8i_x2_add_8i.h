85568d2fea32fbadcdffcaafd1bf5053f194832b
Marcus Alagar <mvala079@gmail.com>
2025-07-29 23:05:57 -0500

    Actually x2 fix RVV sum of byte vectors
    Hopefully this time actually fix by casting from
    `char*` to `signed char*`.

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_8i_x2_add_8i.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_8i_x2_add_8i.h
@@ -212,9 +212,11 @@ static inline void volk_gnsssdr_8i_x2_add_8i_rvv(char* cVector, const char* aVec
     size_t n = num_points;
 
     // Initialize pointers to track progress as stripmine
-    signed char* cPtr = cVector; // For consistency
-    const signed char* aPtr = aVector;
-    const signed char* bPtr = bVector;
+    // Macro expects `int8_t`, and `char`'s signedness
+    // depends on implementation
+    signed char* cPtr = (signed char*) cVector; // For consistency
+    const signed char* aPtr = (const signed char*) aVector;
+    const signed char* bPtr = (const signed char*) bVector;
 
     for (size_t vl; n > 0; n -= vl, cPtr += vl, aPtr += vl, bPtr += vl)
         {
