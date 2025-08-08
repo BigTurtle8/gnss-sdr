f1476d3e4ae9e5991f640f8ab72bcbd7880f48f9
Marcus Alagar <mvala079@gmail.com>
2025-08-08 01:59:49 -0500

    Fix accidental swapping of types in intrinsic name

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_8ic_x2_multiply_8ic.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_8ic_x2_multiply_8ic.h
@@ -327,10 +327,10 @@ static inline void volk_gnsssdr_8ic_x2_multiply_8ic_rvv(lv_8sc_t* cVector, const
         cImagVal = __riscv_vmacc_vv_i8m4(cImagVal, aImagVal, bRealVal, vl);
 
         // Store cReal[0..vl), cImag[0..vl)
-        vint8m4x2_t cVal = __riscv_vset_v_i8m4x2_i8m4(
+        vint8m4x2_t cVal = __riscv_vset_v_i8m4_i8m4x2(
             __riscv_vundefined_i8m4x2(), 0, cRealVal
         );
-        cVal = __riscv_vset_v_i8m4x2_i8m4(cVal, 1, cImagVal);
+        cVal = __riscv_vset_v_i8m4_i8m4x2(cVal, 1, cImagVal);
         __riscv_vsseg2e8_v_i8m4x2(cPtr, cVal, vl);
 
         // In looping, decrement the number of
