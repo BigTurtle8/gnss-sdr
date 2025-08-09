5652708c2c8ca4020d7401ab2d15e223a432424f
Marcus Alagar <mvala079@gmail.com>
2025-08-08 22:12:57 -0500

    Implement RVV conjugate
    Implement calculating the conjugate for every element in a
    vector of complex numbers using RVV C inrinsics in
    `volk_gnsssdr_8ic_conjugate_8ic_rvv`.

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_8ic_conjugate_8ic.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_8ic_conjugate_8ic.h
@@ -352,4 +352,48 @@ static inline void volk_gnsssdr_8ic_conjugate_8ic_neon(lv_8sc_t* cVector, const 
 }
 #endif /* LV_HAVE_NEON */
 
+
+#ifdef LV_HAVE_RVV
+#include <riscv_vector.h>
+
+static inline void volk_gnsssdr_8ic_conjugate_8ic_rvv(lv_8sc_t* cVector, const lv_8sc_t* aVector, unsigned int num_points) {
+    size_t n = num_points;
+
+    // Initialize pointers to track progress as stripmine
+    // Assuming that intended to use `signed char`,
+    // as `char`'s signedness is implementation-specific
+    signed char* cPtr = (signed char*) cVector;
+    const signed char* aPtr = (const signed char*) aVector;
+
+    // Helper constants for clarity
+    const size_t REAL_I = 0, IMAG_I = 1;
+
+    for (size_t vl; n > 0; n -= vl, cPtr += vl * 2, aPtr += vl * 2) {
+        // Record how many elements will actually be processed
+        vl = __riscv_vsetvl_e8m4(n);
+
+        // Load aReal[0..vl), aImag[0..vl)
+        vint8m4x2_t aVal = __riscv_vlseg2e8_v_i8m4x2(aPtr, vl);
+        vint8m4_t aRealVal = __riscv_vget_v_i8m4x2_i8m4(aVal, REAL_I);
+        vint8m4_t aImagVal = __riscv_vget_v_i8m4x2_i8m4(aVal, IMAG_I);
+
+        // negImag[i] = -aImag[0..vl)
+        vint8m4_t negImagVal = __riscv_vneg_v_i8m4(aImagVal, vl);
+
+        // Store aReal[0..vl), negImag[0..vl) into `cPtr`
+        vint8m4x2_t cVal = __riscv_vset_v_i8m4_i8m4x2(
+            __riscv_vundefined_i8m4x2(), REAL_I, aRealVal
+        );
+        cVal = __riscv_vset_v_i8m4_i8m4x2(cVal, IMAG_I, negImagVal);
+        __riscv_vsseg2e8_v_i8m4x2(cPtr, cVal, vl);
+
+        // In looping, decrease the number of
+        // elements left and increase the pointers
+        // by the number of elements processed,
+        // taking into account how each complex number
+        // stored in `cPtr` and `aPtr` is two 1-byte chars
+    }
+}
+#endif /* LV_HAVE_RVV */
+
 #endif /* INCLUDED_volk_gnsssdr_8ic_conjugate_8ic_H */
