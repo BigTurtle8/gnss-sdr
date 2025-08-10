f52e718b11c377a5ccaf40d196801641029d5909
Marcus Alagar <mvala079@gmail.com>
2025-08-10 14:07:17 -0500

    Fix silly mismatch in function and desired type

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_16ic_convert_32fc.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_16ic_convert_32fc.h
@@ -284,7 +284,7 @@ static inline void volk_gnsssdr_16ic_convert_32fc_rvv(lv_32fc_t* outputVector, c
             vl = __riscv_vsetvl_e16m2(vl);
 
             // Load inReal[0..vl), inImag[0..vl)
-            vint16m2x2_t inVal = __riscv_vlseg2e16_v_i16m4x2(inPtr, vl);
+            vint16m2x2_t inVal = __riscv_vlseg2e16_v_i16m2x2(inPtr, vl);
             vint16m2_t inRealVal = __riscv_vget_v_i16m4x2_i16m4(inVal, 0);
             vint16m2_t inImagVal = __riscv_vget_v_i16m4x2_i16m4(inVal, 1);
 
