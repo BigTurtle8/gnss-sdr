a75130d1e8e9eb069c02b6c526f863b5e918f5e6
Marcus Alagar <mvala079@gmail.com>
2025-08-12 03:35:32 -0500

    Try not narrowing and converting at the same time
    Try not narrowing from 32 bits to 16 bits and converting from floating
    point to integer at the same time.

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_32fc_convert_8ic.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_32fc_convert_8ic.h
@@ -476,12 +476,14 @@ static inline void volk_gnsssdr_32fc_convert_8ic_rvv(lv_8sc_t* outputVector, con
             vfloat32m4_t inImagVal = __riscv_vget_v_f32m4x2_f32m4(inVal, 1);
 
             // outReal[i] = (signed char) inReal[i]
-            vint16m2_t tmpRealVal = __riscv_vfncvt_rtz_x_f_w_i16m2(inRealVal, vl);
-            vint8m1_t outRealVal = __riscv_vncvt_x_x_w_i8m1(tmpRealVal, vl);
+            vint32m4_t tmp32RealVal = __riscv_vfcvt_x_f_v_i32m4(inRealVal, vl);
+            vint16m2_t tmp16RealVal = __riscv_vncvt_x_x_w_i16m2(tmp32RealVal, vl);
+            vint8m1_t outRealVal = __riscv_vncvt_x_x_w_i8m1(tmp16RealVal, vl);
 
             // outImag[i] = (signed char) inImag[i]
-            vint16m2_t tmpImagVal = __riscv_vfncvt_rtz_x_f_w_i16m2(inImagVal, vl);
-            vint8m1_t outImagVal = __riscv_vncvt_x_x_w_i8m1(tmpImagVal, vl);
+            vint32m4_t tmp32ImagVal = __riscv_vfcvt_x_f_v_i32m4(inImagVal, vl);
+            vint16m2_t tmp16ImagVal = __riscv_vncvt_x_x_w_i16m2(tmp32ImagVal, vl);
+            vint8m1_t outImagVal = __riscv_vncvt_x_x_w_i8m1(tmp16ImagVal, vl);
 
             // Store outReal[0..vl), outImag[0..vl)
             vint8m1x2_t outVal = __riscv_vset_v_i8m1_i8m1x2(
