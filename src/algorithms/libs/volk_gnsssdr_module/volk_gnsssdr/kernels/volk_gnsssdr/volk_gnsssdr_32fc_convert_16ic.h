1b0a5904bf6df269de8637d6723caeeed677f3fe
Marcus Alagar <mvala079@gmail.com>
2025-08-20 16:27:36 -0500

    Remove commented out lines
    Remove commented out lines from previous copy-paste implementation
    of converting 32-bit floating-point complex numbers to 8-bit integer
    complex numbers.

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_32fc_convert_16ic.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_32fc_convert_16ic.h
@@ -483,15 +483,9 @@ static inline void volk_gnsssdr_32fc_convert_16ic_rvv(lv_16sc_t* outputVector, c
             vfloat32m4_t inImagVal = __riscv_vget_v_f32m4x2_f32m4(inVal, 1);
 
             // outReal[i] = (short) inReal[i]
-            //vint32m4_t tmp32RealVal = __riscv_vfcvt_x_f_v_i32m4(inRealVal, vl);
-            //vint16m2_t tmp16RealVal = __riscv_vncvt_x_x_w_i16m2(tmp32RealVal, vl);
-            //vint8m1_t outRealVal = __riscv_vncvt_x_x_w_i8m1(tmp16RealVal, vl);
             vint16m2_t outRealVal = __riscv_vfncvt_x_f_w_i16m2(inRealVal, vl);
 
             // outImag[i] = (short) inImag[i]
-            //vint32m4_t tmp32ImagVal = __riscv_vfcvt_x_f_v_i32m4(inImagVal, vl);
-            //vint16m2_t tmp16ImagVal = __riscv_vncvt_x_x_w_i16m2(tmp32ImagVal, vl);
-            //vint8m1_t outImagVal = __riscv_vncvt_x_x_w_i8m1(tmp16ImagVal, vl);
             vint16m2_t outImagVal = __riscv_vfncvt_x_f_w_i16m2(inImagVal, vl);
 
             // Store outReal[0..vl), outImag[0..vl)
