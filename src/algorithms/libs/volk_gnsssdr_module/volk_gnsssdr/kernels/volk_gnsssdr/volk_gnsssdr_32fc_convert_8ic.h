38223e55e1b2de3575857a5c3b74c36722f26951
Marcus Alagar <mvala079@gmail.com>
2025-08-24 20:44:25 -0500

    Fix silly typo with new optimization
    Fix silly typo with new optimization where changed types
    but not associated function.

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_32fc_convert_8ic.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_32fc_convert_8ic.h
@@ -483,12 +483,12 @@ static inline void volk_gnsssdr_32fc_convert_8ic_rvv(lv_8sc_t* outputVector, con
             vfloat32m8_t tmp32Val = __riscv_vfmul_vf_f32m8(inVal, (float) 127, vl);
 
             // Saturate tmp[i] to 8 bits
-            tmp32Val = __riscv_vfmin_vf_f32m4(tmp32Val, (float) 127, vl);
-            tmp32Val = __riscv_vfmax_vf_f32m4(tmp32Val, (float) -128, vl);
+            tmp32Val = __riscv_vfmin_vf_f32m8(tmp32Val, (float) 127, vl);
+            tmp32Val = __riscv_vfmax_vf_f32m8(tmp32Val, (float) -128, vl);
 
             // out[i] = (signed char) tmp[i]
-            vint16m4_t tmp16Val = __riscv_vfncvt_x_f_w_i16m2(tmp32Val, vl);
-            vint8m2_t outVal = __riscv_vncvt_x_x_w_i8m1(tmp16Val, vl);
+            vint16m4_t tmp16Val = __riscv_vfncvt_x_f_w_i16m4(tmp32Val, vl);
+            vint8m2_t outVal = __riscv_vncvt_x_x_w_i8m2(tmp16Val, vl);
 
             // Store out[0..vl)
             __riscv_vse8_v_i8m2(outPtr, outVal, vl);
