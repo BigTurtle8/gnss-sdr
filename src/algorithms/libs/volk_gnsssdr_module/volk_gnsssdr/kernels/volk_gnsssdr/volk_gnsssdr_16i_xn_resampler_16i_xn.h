198692731a01193a043d55dd2f7aa3f3fd8059a9
Marcus Alagar <mvala079@gmail.com>
2025-08-25 23:56:20 -0500

    Experimentally optimize xn resampler again
    Experimentally optimize xn resampler by moving all computations from
    out of the manual for loop into the vectorized computations
    using RVV.

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_16i_xn_resampler_16i_xn.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_16i_xn_resampler_16i_xn.h
@@ -607,6 +607,7 @@ static inline void volk_gnsssdr_16i_xn_resampler_16i_xn_rvv(int16_t** result, co
 
     for (int current_correlator_tap = 0; current_correlator_tap < num_out_vectors; current_correlator_tap++)
         {
+            /*
             // Stores indices for `local_code` to load from and
             // then store in `result[current_correlator_tap`
             // These indices may overflow and will wrap into
@@ -622,14 +623,19 @@ static inline void volk_gnsssdr_16i_xn_resampler_16i_xn_rvv(int16_t** result, co
 
                     overflowIndexBuffer[i] = (unsigned int) local_code_chip_index;
                 }
+            */
 
             size_t n = num_points;
 
-            // Initialize pointers to track progress as stripmine
-            int16_t* outPtr = result[current_correlator_tap];
-            const unsigned int* overflowIndexPtr = (const unsigned int*) overflowIndexBuffer;
+            const float constIndexShift = shifts_chips[current_correlator_tap] - rem_code_phase_chips;
 
-            for (size_t vl; n > 0; n -= vl, outPtr += vl, overflowIndexPtr += vl)
+            // Initialize pointer, counter to track progress as stripmine
+            int16_t* outPtr = result[current_correlator_tap];
+            // Simulates how, compared to generic implementation, `i` continues
+            // increasing across different vector computatation batches
+            unsigned int currI = 0;
+
+            for (size_t vl; n > 0; n -= vl, outPtr += vl, currI += vl)
                 {
                     // Use LMUL of 4 since EEW/SEW of indexed load is 2,
                     // meaning that to use u32 as indices to load
@@ -637,16 +643,31 @@ static inline void volk_gnsssdr_16i_xn_resampler_16i_xn_rvv(int16_t** result, co
                     // Record how many data elements will actually be processed
                     vl = __riscv_vsetvl_e16m4(n);
 
-                    // Load overflowIndex[0..vl)
-                    vuint32m8_t overflowIndexVal = __riscv_vle32_v_u32m8(overflowIndexPtr, vl);
+                    // floatI[i] = (float) (i + currI)
+                    vuint32m8_t idVal = __riscv_vid_v_u32m8(vl);
+                    vuint32m8_t iVal = __riscv_vadd_vx_u32m8(idVal, currI, vl);
+                    vfloat32m8_t floatIVal = __riscv_vfcvt_f_xu_v_f32m8(iVal, vl);
 
-                    // Wrap to valid index in `local_code`
-                    // index[i] = overflowIndex[i] % code_length_chips
-                    vuint32m8_t indexVal = __riscv_vremu_vx_u32m8(overflowIndexVal, code_length_chips, vl);
+                    // iterIndex[i] = floatIVal[i] * code_phase_step_chips
+                    vfloat32m8_t iterIndexVal = __riscv_vfmul_vf_f32m8(floatIVal, code_phase_step_chips, vl);
+
+                    // overflowIndex[i] = (int) floor(iterIndex[i] + constIndexShift)
+                    vfloat32m8_t shiftedIndexVal = __riscv_vfadd_vf_f32m8(iterIndexVal, constIndexShift, vl);
+                    vint32m8_t overflowIndexVal = __riscv_vfcvt_x_f_v_i32m8_rm(shiftedIndexVal, __RISCV_FRM_RDN, vl);
+
+                    // Wrap to valid index in `local_code`, handling negative values
+                    // index[i] = ( code_length_chips + ( overflowIndex[i] % code_length_chips ) ) % code_length_chips
+                    vint32m8_t indexVal = __riscv_vrem_vx_i32m8(overflowIndexVal, code_length_chips, vl);
+                    indexVal = __riscv_vadd_vx_i32m8(indexVal, code_length_chips, vl);
+                    indexVal = __riscv_vrem_vx_i32m8(indexVal, code_length_chips, vl);
+
+                    // After above, should now be guaranteed positive and valid index
+                    // finalIndex[i] = (unsigned int) index[i];
+                    vuint32m8_t finalIndexVal = __riscv_vreinterpret_v_i32m8_u32m8(indexVal);
 
                     // Convert to address offset
-                    // offset[i] = index[i] * sizeof(int16_t)
-                    vuint32m8_t offsetVal = __riscv_vmul_vx_u32m8(indexVal, sizeof(int16_t), vl);
+                    // offset[i] = finalIndex[i] * sizeof(int16_t)
+                    vuint32m8_t offsetVal = __riscv_vmul_vx_u32m8(finalIndexVal, sizeof(int16_t), vl);
 
                     // This indexed load is unordered to hopefully boost run time
                     // out[i] = in[offset[i]]
