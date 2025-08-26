a577df5f2332cc198e8354fc628094a125bacf0a
Marcus Alagar <mvala079@gmail.com>
2025-08-26 14:24:08 -0500

    Fix unclear comment mistakes in xn resamplers

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_32fc_xn_resampler_32fc_xn.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_32fc_xn_resampler_32fc_xn.h
@@ -788,7 +788,7 @@ static inline void volk_gnsssdr_32fc_xn_resampler_32fc_xn_rvv(lv_32fc_t** result
                     vuint32m4_t iVal = __riscv_vadd_vx_u32m4(idVal, currI, vl);
                     vfloat32m4_t floatIVal = __riscv_vfcvt_f_xu_v_f32m4(iVal, vl);
 
-                    // iterIndex[i] = floatIVal[i] * code_phase_step_chips
+                    // iterIndex[i] = floatI[i] * code_phase_step_chips
                     vfloat32m4_t iterIndexVal = __riscv_vfmul_vf_f32m4(floatIVal, code_phase_step_chips, vl);
 
                     // overflowIndex[i] = (int) floor(iterIndex[i] + constIndexShift)
@@ -817,7 +817,7 @@ static inline void volk_gnsssdr_32fc_xn_resampler_32fc_xn_rvv(lv_32fc_t** result
                     __riscv_vse64_v_i64m8(outPtr, outVal, vl);
 
                     // In looping, decrement the number of
-                    // elements left and increment stripmining pointers
+                    // elements left and increment stripmining variables
                     // by the number of elements processed
                 }
         }
