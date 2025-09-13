41f09eb797680f0b5d130e472b20fb2edfed7641
Marcus Alagar <mvala079@gmail.com>
2025-09-13 11:09:43 -0700

    Update github actions to next, force update
    Update github actions to level of files in `next`, and then
    force update.

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_s32f_sincospuppet_32fc.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_s32f_sincospuppet_32fc.h
@@ -102,7 +102,7 @@ static inline void volk_gnsssdr_s32f_sincospuppet_32fc_neon(lv_32fc_t* out, cons
 #ifdef LV_HAVE_RVV
 static inline void volk_gnsssdr_s32f_sincospuppet_32fc_rvv(lv_32fc_t* out, const float phase_inc, unsigned int num_points)
 {
-    // REMOVE THIS COMMENT - FORCE UPDATE #2
+    // REMOVE THIS COMMENT - FORCE UPDATE #3
     float phase[1];
     phase[0] = 3;
     volk_gnsssdr_s32f_sincos_32fc_rvv(out, phase_inc, phase, num_points);
