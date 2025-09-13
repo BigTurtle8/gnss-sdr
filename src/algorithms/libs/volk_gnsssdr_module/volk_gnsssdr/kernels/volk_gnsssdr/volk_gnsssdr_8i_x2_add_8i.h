27b74cae3c497791f25c8c1468896655c3dc6cc6
Marcus Alagar <mvala079@gmail.com>
2025-09-13 15:33:14 -0700

    Fix comment spacing for cpplint

--- src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_8i_x2_add_8i.h
+++ src/algorithms/libs/volk_gnsssdr_module/volk_gnsssdr/kernels/volk_gnsssdr/volk_gnsssdr_8i_x2_add_8i.h
@@ -215,7 +215,7 @@ static inline void volk_gnsssdr_8i_x2_add_8i_rvv(char* cVector, const char* aVec
     // Initialize pointers to track progress as stripmine
     // Macro expects `int8_t`, and `char`'s signedness
     // depends on implementation
-    signed char* cPtr = (signed char*) cVector; // For consistency
+    signed char* cPtr = (signed char*) cVector;  // For consistency
     const signed char* aPtr = (const signed char*) aVector;
     const signed char* bPtr = (const signed char*) bVector;
 
